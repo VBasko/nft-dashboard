ae736c870b04331d955ae078af830c77c1cc1e0d
VBasko <vvbasko@outlook.com>
2024-03-18 23:43:33 -0300

    fix: nuxt-svgo package uninstalled with its config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["nuxt-svgo", "@nuxtjs/tailwindcss"],
-  svgo: {
-    componentPrefix: "Icon",
-  },
+  modules: ["@nuxtjs/tailwindcss"],
 });
