ef773a41cc9474fb7df81fd8474455b4fdd33dcc
VBasko <vvbasko@outlook.com>
2024-03-22 17:31:34 -0300

    feat: add Slider to auth layout

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -1,8 +1,18 @@
 <template>
-  <div class="min-h-screen">
-    <div class="left-part">
-      <IconsLogoSmall />
+  <div class="min-h-screen flex">
+    <div class="flex-1 p-4 flex flex-col justify-between">
+      <NuxtLink to="/" class="self-start">
+        <IconsLogoSmall />
+      </NuxtLink>
+
+      <slot />
+      <ul class="flex justify-between">
+        <li><a href="#">Privacy Policy</a></li>
+        <li><a href="#">Copyright 2022</a></li>
+      </ul>
     </div>
-    <slot />
+    <div class="flex-1 p-4 bg-black">
+      <Slider />
+    </div>
   </div>
 </template>
