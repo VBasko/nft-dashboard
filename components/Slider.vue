a39650f49a974aee665b5d7ff4263892f9e4cbae
VBasko <vvbasko@outlook.com>
2024-03-22 21:40:42 -0300

    refactor: Slider component, auth layout styles from tailwind to scss

--- components/Slider.vue
+++ components/Slider.vue
@@ -1,30 +1,43 @@
 <template>
-  <div class="flex flex-col items-center py-32 text-center">
+  <div class="carousel">
     <div
       v-for="(slide, i) in slides"
       :key="i"
       v-show="i == currentSlide"
-      class="max-w-sm"
+      class="carousel__slide slide"
     >
-      <div class="relative flex justify-center mb-16">
+      <div class="slide__image-container">
         <img
-          :src="`_nuxt/${slide.src}`"
+          :src="`_nuxt/assets/images/${slide.src}`"
           :alt="slide.alt"
-          class="aspect-square object-cover rounded-3xl absolute z-10"
+          class="slide__image"
         />
         <img
-          :src="`_nuxt/${slide.src}`"
+          :src="`_nuxt/assets/images/${slide.src}`"
           :alt="slide.alt"
-          class="clone aspect-square object-cover rounded-3xl top-5 opacity-70 blur-3xl"
+          class="slide__image slide__image--clone"
         />
       </div>
-      <h2 class="text-white font-bold text-5xl mb-3.5">{{ slide.title }}</h2>
-      <p class="text-[#6d6d6d] text-base mb-16">{{ slide.description }}</p>
+      <h2 class="slide__title">{{ slide.title }}</h2>
+      <p class="slide__description">{{ slide.description }}</p>
     </div>
 
-    <div class="mt-4 flex gap-2">
-      <button class="bg-white p-2" @click="prevSlide">Prev</button>
-      <button class="bg-white p-2" @click="nextSlide">Next</button>
+    <div class="carousel__controls">
+      <button
+        aria-label="Previous slide"
+        class="carousel__button"
+        @click="prevSlide"
+      >
+        <IconsArrowLeft />
+      </button>
+      <span class="carousel__separator"></span>
+      <button
+        aria-label="Next slide"
+        class="carousel__button"
+        @click="nextSlide"
+      >
+        <IconsArrowRight />
+      </button>
     </div>
   </div>
 </template>
@@ -39,27 +52,27 @@ interface Slide {
 
 const slides = ref<Slide[]>([
   {
-    src: "assets/slide-1.jpg",
+    src: "slide-1.jpg",
     alt: "sun in the sky during daytime",
     title: "Uncover Rare NFT Gems",
     description:
       "Embark on a journey to discover the rarest NFT gems on DiveSea!",
   },
   {
-    src: "assets/slide-2.jpg",
+    src: "slide-2.jpg",
     alt: "blue and white round illustration",
     title: "Curate Your NFT Gallery",
     description:
       "Create your own NFT gallery and showcase unique digital art on DiveSea!",
   },
   {
-    src: "assets/slide-3.jpg",
+    src: "slide-3.jpg",
     alt: "a blue and pink background with a circle",
     title: "Immerse in Artistic Wonders",
     description: "Explore a world of artistic wonders through NFTs on DiveSea!",
   },
   {
-    src: "assets/slide-4.jpg",
+    src: "slide-4.jpg",
     alt: "white and brown round frame",
     title: "Discover the Artistic Universe",
     description: "Dive into the vast universe of art through NFTs on DiveSea!",
@@ -77,3 +90,76 @@ const prevSlide = (): void => {
     (currentSlide.value - 1 + slides.value.length) % slides.value.length;
 };
 </script>
+
+<style scoped lang="scss">
+.carousel {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  row-gap: 4rem;
+  padding: 4rem;
+  text-align: center;
+
+  &__slide {
+    max-width: 24rem;
+  }
+
+  &__controls {
+    display: flex;
+    align-items: center;
+    background-color: #ffffff;
+    border-radius: 18px;
+  }
+
+  &__button {
+    padding: 1.5rem;
+    color: #929292;
+
+    &:hover {
+      color: #23262f;
+    }
+  }
+
+  &__separator {
+    width: 2px;
+    height: 28px;
+    background-color: #e6e8ec;
+  }
+}
+
+.slide {
+  &__image {
+    aspect-ratio: 1 / 1;
+    object-fit: cover;
+    border-radius: 28px;
+
+    &--clone {
+      position: absolute;
+      top: 1.25rem;
+      opacity: 0.7;
+      filter: blur(5rem);
+      z-index: -1;
+    }
+
+    &-container {
+      position: relative;
+      display: flex;
+      justify-content: center;
+      margin-bottom: 4rem;
+      z-index: 1;
+    }
+  }
+
+  &__title {
+    color: #ffffff;
+    font-weight: 700;
+    font-size: 3rem;
+    margin-bottom: 0.875rem;
+  }
+
+  &__description {
+    font-size: 1rem;
+    color: #6d6d6d;
+  }
+}
+</style>
--- layouts/auth.vue
+++ layouts/auth.vue
@@ -1,18 +1,58 @@
 <template>
-  <div class="min-h-screen flex">
-    <div class="flex-1 p-4 flex flex-col justify-between">
-      <NuxtLink to="/" class="self-start">
+  <div class="l-flex auth">
+    <div class="l-flex__half auth__left">
+      <NuxtLink to="/" class="auth__header">
         <IconsLogoSmall />
       </NuxtLink>
 
       <slot />
-      <ul class="flex justify-between">
-        <li><a href="#">Privacy Policy</a></li>
-        <li><a href="#">Copyright 2022</a></li>
+      <ul class="auth__footer">
+        <li class="auth__footer-item">
+          <a href="#" class="auth__footer-link">Privacy Policy</a>
+        </li>
+        <li class="auth__footer-item">Copyright 2022</li>
       </ul>
     </div>
-    <div class="flex-1 p-4 bg-black">
+    <div class="l-flex__half auth__right">
       <Slider />
     </div>
   </div>
 </template>
+
+<style scoped lang="scss">
+.l-flex {
+  min-height: 100vh;
+  display: flex;
+
+  &__half {
+    flex: 1 1 0%;
+    padding: 1.5rem 2.75rem;
+  }
+}
+
+.auth {
+  &__left {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+
+  &__right {
+    background-color: var(--color-black);
+  }
+
+  &__header {
+    align-self: start;
+  }
+
+  &__footer {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    &-item {
+      color: #aaaaaa;
+    }
+  }
+}
+</style>
